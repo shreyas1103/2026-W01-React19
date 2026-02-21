cec3c4c5f9aa9cbd6b6729d8b789c6522e19eaa5
shreyas1103 <shreyashmagdum02@gmail.com>
2026-02-21 16:42:02 +0530

    Changes made

--- REACT JS/script9.js
+++ REACT JS/script9.js
@@ -16,3 +16,11 @@ console.log(_skills);
 // add on more skill to employee
 employees = {...employees, skills : [...employees.skills , "react"]}  // is read as spread operator used to copy array and object
 console.log(employees);
+
+// replace tester with enginner
+employees = {...employees, role : employees.role.map(item => item.position === "tester" ? {...item, position : "engineer"} : item)}  // is read as spread operator used to copy array and object
+console.log(employees);
+
+// another way 
+employees = {...employees,role:[...employees.role , employees.role[1].position = "engineer"]}  // is read as spread operator used to copy array and object
+console.log(employees);
